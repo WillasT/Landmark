0cef1e02506f88b9b039037acfd6b9c757b34115
Willas <wrorrong@student.ciputra.ac.id>
2024-04-19 10:34:51 +0700

    Section 2 composing complex

--- Landmarks/Resources/ModelData.swift
+++ Landmarks/Resources/ModelData.swift
@@ -12,6 +12,12 @@ class ModelData{
     var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
 
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 
